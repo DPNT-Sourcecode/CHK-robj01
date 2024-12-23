097b1b862505f5f890ac3a13742eed3ef1108456
sbx_user1051 <sbx_user1051@169.254.23.70>
2024-12-23 08:02:11 +0000

    Mon Dec 23 08:02:11 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,9 +5,17 @@ from collections import Counter
 def checkout(skus):
     price_table = {"A": {"price": 50, "offer": {"count": 3, "price": 130}},
                    "B": {"price": 30, "offer": {"count": 2, "price": 45}},
-                   "C": {"price": 50, "offer": None},
-                   "D": 15}
+                   "C": {"price": 20, "offer": None},
+                   "D": {"price": 15, "offer": None}}
+    total_price = 0
+
     skus_counter = Counter(skus)
-    print(skus_counter)
+
+    for item, count in skus_counter.items():
+        if item not in price_table.keys(): return -1
+
+        total_price += price_table[item]["price"] * count
+
     return None
+
 
