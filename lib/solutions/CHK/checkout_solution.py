98ff0dd1869e511d898ab48256f4ef87161de38e
sbx_user1051 <sbx_user1051@169.254.23.70>
2024-12-23 09:48:19 +0000

    Mon Dec 23 09:48:19 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,80 +2,86 @@ from collections import Counter
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-class item:
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-        self.count = 0
 
-    class cross_offer:
-        @staticmethod
-        def get():
-            return None
 
-    class offer:
-        @staticmethod
-        def get():
-            return None
-
-class A(item):
-    def __init__(self):
-        super().__init__("A", 50)
-
-    class offer:
-        @staticmethod
-        def get():
-            count = A.count
-            total_item_value = 0
-
-            total_item_value += (count//5) * 200
-
-            count = count%5
-            total_item_value += (count//3) * 130
-
-            count = count%3
-            total_item_value += count * 50
-            return total_item_value
-class B(item):
-    def __init__(self):
-        super().__init__("B", 30)
-
-    class offer:
-        @staticmethod
-        def get(count):
-            total_item_value = 0
-
-            total_item_value += (count//2) * 45
-
-            count = count%2
-            total_item_value += count * 30
-
-            return total_item_value
-
-class C(item):
-    def __init__(self):
-        super().__init__("C", 20)
-
-class D(item):
-    def __init__(self):
-        super().__init__("D", 15)
-
-class E(item):
-    def __init__(self):
-        super().__init__("E", 40)
-
-    class cross_offer:
-        @staticmethod
-        def get():
-            B.count = B.count - 1 if B.count >= 1 else 0
 
 def checkout(skus):
 
-    price_table = {"A": A(),
-                   "B": B(),
-                   "C": C(),
-                   "D": D(),
-                   "E": E()}
+    class item:
+        def __init__(self, name, price):
+            self.name = name
+            self.price = price
+            self.count = 0
+
+        class cross_offer:
+            @staticmethod
+            def get():
+                return None
+
+        class offer:
+            @staticmethod
+            def get():
+                return None
+
+    class A(item):
+        def __init__(self):
+            super().__init__("A", 50)
+
+        class offer:
+            @staticmethod
+            def get():
+                count = A.count
+                total_item_value = 0
+
+                total_item_value += (count // 5) * 200
+
+                count = count % 5
+                total_item_value += (count // 3) * 130
+
+                count = count % 3
+                total_item_value += count * 50
+                return total_item_value
+
+    class B(item):
+        def __init__(self):
+            super().__init__("B", 30)
+
+        class offer:
+            @staticmethod
+            def get(count):
+                total_item_value = 0
+
+                total_item_value += (count // 2) * 45
+
+                count = count % 2
+                total_item_value += count * 30
+
+                return total_item_value
+
+    class C(item):
+        def __init__(self):
+            super().__init__("C", 20)
+
+    class D(item):
+        def __init__(self):
+            super().__init__("D", 15)
+
+    class E(item):
+        def __init__(self):
+            super().__init__("E", 40)
+
+        class cross_offer:
+            @staticmethod
+            def get():
+                B.count = B.count - 1 if B.count >= 1 else 0
+
+    A, B, C, D, E = A(), B(), C(), D(), E()
+
+    price_table = {"A": A,
+                   "B": B,
+                   "C": C,
+                   "D": D,
+                   "E": E}
 
     total_price = 0
 
@@ -87,11 +93,6 @@ def checkout(skus):
         item_object.count = count
 
         print(item_object.count)
-
-
-
-
-
 
 
 
