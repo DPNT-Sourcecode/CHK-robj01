c828675020f26bfe59d8c43f0f150d0843ca736c
sbx_user1051 <sbx_user1051@169.254.23.70>
2024-12-23 07:27:30 +0000

    Mon Dec 23 07:27:30 UTC 2024

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -3,4 +3,4 @@ from lib.solutions.HLO import hello_solution
 
 class TestHello():
     def test_hello(self):
-        assert hello_solution.hello("Ton") == "Hello, World!"
+        assert hello_solution.hello("Ton") == "Hello, Ton!"
