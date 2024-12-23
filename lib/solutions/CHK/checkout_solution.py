ce3d0456f43a84a547ecf47c3c0f50261646d19c
sbx_user1051 <sbx_user1051@169.254.23.70>
2024-12-23 07:59:11 +0000

    Mon Dec 23 07:59:11 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,11 @@ from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    price_table = {"A": {"price": 50, "offer": {"count": 3, "price": 130}},
+                   "B": {"price": 30, "offer": {"count": 2, "price": 45}},
+                   "C": {"price": 50, "offer": None},
+                   "D": 15}
     skus_counter = Counter(skus)
     print(skus_counter)
     return None
+
